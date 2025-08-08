793f4bf46a68b3bf3b52dcec24f3a5478430c83d
Android Studio <devandroid@ort>
2025-08-08 11:43:21 -0300

    correcion de errores

--- src/navigation/appNavigator.js
+++ src/navigation/appNavigator.js
@@ -8,7 +8,6 @@ import SignUp from '../screens/signUp.js';
 import ForgotPassword from '../screens/forgotPassword.js';
 import SuccessfullAction from '../screens/successfullAction.js';
 import ExtraData from '../screens/extraData.js';
-import OnboardingFlow from '../screens/onboardingScreens/OnboardingFlow';
 import BanderHome from '../screens/banderScreens/banderHome';
 import HosterHome from '../screens/hosterScreens/hosterHome';
 import OnBoardingExtraData0 from '../screens/onboardingScreens/onBoardingExtraData0';
@@ -29,7 +28,6 @@ const AppNavigator = () => {
         <Stack.Screen name="ExtraData" component={ExtraData} />
         <Stack.Screen name="ForgotPassword" component={ForgotPassword} />
         <Stack.Screen name="SuccessfullAction" component={SuccessfullAction} />
-        <Stack.Screen name="Onboarding" component={OnboardingFlow} />
         <Stack.Screen name="BanderHome" component={BanderHome} />
         <Stack.Screen name="HosterHome" component={HosterHome} />
         <Stack.Screen name="OnBoardingExtraData0" component={OnBoardingExtraData0} />
